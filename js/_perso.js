c53a28c047356ffb8c26cb77e8552591743e5795
Rayan <mhitoslehero@hotmail.com>
2018-08-13 16:09:55 +0200

    4eme commit fin des methodes attaque

--- js/_perso.js
+++ js/_perso.js
@@ -1,7 +1,7 @@
-var manaTab = [7,9,11];
+var manaTab = [7, 9, 11];
 
 export class Boss {
-  constructor(nom, vie, attaque){
+  constructor(nom, vie, attaque) {
     this.nom = nom;
     this.vie = vie;
     this.attaque = attaque;
@@ -9,86 +9,106 @@ export class Boss {
 };
 
 export class Guerrier {
-  constructor(nom, vie, attaque){
+  constructor(nom, vie, attaque) {
     this.nom = nom;
     this.vie = vie;
     this.attaque = attaque;
     this.rage = 0;
-  };
-  postures(){
-    var posture = prompt("Choisissez votre posture ! Attaque (taper 0), Défense (taper 1), normal (taper 2)")
-    if (posture == "0"){
-      this.vie = this.vie * 0.75;
-      this.attaque = this.attaque * 1.4;
+  }
+  attaque(){
+    Boss.vie -= this.attaque;
+    if (this.rage < 4){
+      this.rage ++;
     }
-    if (posture == "1"){
-      this.vie = this.vie * 2.5;
-      this.attaque = this.attaque * 0.5;
-    }
-    if (posture == "2"){
-      this.vie = this.vie;
-      this.attaque = this.attaque;
+    else if (this.rage = 4){
+      this.attaque = this.attaque * 1.25;
     }
     else {
-      alert("Vous devez choisir 0, 1 ou 2 !")
-      postures();
+      this.rage = 0;
     }
-  } 
+  };
+  postures() {
+    var posture = prompt("Choisissez votre posture Gerrier ! Attaque (taper 0), Défense (taper 1), normal (taper 2)")
+    if (posture == "0") {
+      this.vie = this.vie * 0.75;
+      this.attaque = this.attaque * 1.4;
+    } else if (posture == "1") {
+      this.vie = this.vie * 2.5;
+      this.attaque = this.attaque * 0.5;
+    } else if (posture == "2") {
+      this.vie = this.vie;
+      this.attaque = this.attaque;
+    } else {
+      alert("Vous devez choisir 0, 1 ou 2 !")
+      this.postures();
+    }
+  }
 };
 
 export class Archer {
-  constructor(nom, vie, attaque){
+  constructor(nom, vie, attaque) {
     this.nom = nom;
     this.vie = vie;
     this.attaque = attaque;
     this.fleche = Math.floor(Math.random() * 5) + 7;
   }
-  postures(){
-    var posture = prompt("Choisissez votre posture ! Attaque (taper 0), Défense (taper 1), normal (taper 2)")
-    if (posture == "0"){
-      this.vie = this.vie * 0.75;
-      this.attaque = this.attaque * 1.4;
-    }
-    if (posture == "1"){
-      this.vie = this.vie * 2.5;
-      this.attaque = this.attaque * 0.5;
-    }
-    if (posture == "2"){
-      this.vie = this.vie;
-      this.attaque = this.attaque;
+  attaque(){
+    if (this.fleche > 1){
+      Boss.vie -= this.attaque;
+      this.fleche = this.fleche -1;
     }
     else {
+      this.fleche = this.fleche + 6;
+    }
+  }
+  postures() {
+    var posture = prompt("Choisissez votre posture pour Archer ! Attaque (taper 0), Défense (taper 1), normal (taper 2)")
+    if (posture == "0") {
+      this.vie = this.vie * 0.75;
+      this.attaque = this.attaque * 1.4;
+    } else if (posture == "1") {
+      this.vie = this.vie * 2.5;
+      this.attaque = this.attaque * 0.5;
+    } else if (posture == "2") {
+      this.vie = this.vie;
+      this.attaque = this.attaque;
+    } else {
       alert("Vous devez choisir 0, 1 ou 2 !")
-      postures();
+      this.postures();
     }
   }
 };
 
 export class Mage {
-  constructor(nom, vie, attaque){
+  constructor(nom, vie, attaque) {
     this.nom = nom;
     this.vie = vie;
     this.attaque = attaque;
     this.mana = manaTab[Math.floor(manaTab.length * Math.random())];
-}
-postures(){
-  var posture = prompt("Choisissez votre posture ! Attaque (taper 0), Défense (taper 1), normal (taper 2)")
-  if (posture == "0"){
-    this.vie = this.vie * 0.75;
-    this.attaque = this.attaque * 1.4;
   }
-  if (posture == "1"){
-    this.vie = this.vie * 2.5;
-    this.attaque = this.attaque * 0.5;
+  attaque(){
+    if (this.mana > 2){
+      Boss.vie -= this.attaque;
+      this.mana = this.mana - 2
+    }
+    else {
+      this.mana = this.mana + 7;
+    }
   }
-  if (posture == "2"){
-    this.vie = this.vie;
-    this.attaque = this.attaque;
+  postures() {
+    var posture = prompt("Choisissez votre posture pour Mage ! Attaque (taper 0), Défense (taper 1), normal (taper 2)")
+    if (posture == "0") {
+      this.vie = this.vie * 0.75;
+      this.attaque = this.attaque * 1.4;
+    } else if (posture == "1") {
+      this.vie = this.vie * 2.5;
+      this.attaque = this.attaque * 0.5;
+    } else if (posture == "2") {
+      this.vie = this.vie;
+      this.attaque = this.attaque;
+    } else {
+      alert("Vous devez choisir 0, 1 ou 2 !")
+      this.postures();
+    }
   }
-  else {
-    alert("Vous devez choisir 0, 1 ou 2 !")
-    postures();
-  }
-}
 };
-
